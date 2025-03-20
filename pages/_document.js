7e2a83aecf6f4a51b6e2678cb8cc2a3503721318
gr82b4tun8 <gr82b4tun8@gmail.com>
2025-03-20 14:30:08 +0000

    fixed ads??

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,20 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-        {/* Google AdSense Code - placed at the end of body */}
-        <Script
+      <Head>
+        {/* eslint-disable-next-line */}
+        <script
           async
-          strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1549212779236114"
           crossOrigin="anonymous"
         />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   )
