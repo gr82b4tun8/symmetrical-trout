feba4195e6666ccfe2502e01fa9f7f6ba9983100
gr82b4tun8 <gr82b4tun8@gmail.com>
2025-03-20 14:56:46 +0000

    fixed ads?????

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import "@/styles/globals.css";
 import Script from 'next/script';
 
+// Correctly integrate the Google AdSense component
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-// components/GoogleAdSense.js for auto ads only
-export default function GoogleAdsense() {
   return (
-    <Script
-      id="adsbygoogle-init"
-      strategy="afterInteractive"
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1549212779236114"
-      crossOrigin="anonymous"
-    />
+    <>
+      {/* Google AdSense Script */}
+      <Script
+        id="adsbygoogle-init"
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1549212779236114"
+        crossOrigin="anonymous"
+      />
+      
+      {/* Main application component */}
+      <Component {...pageProps} />
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head>
-        {/* eslint-disable-next-line */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1549212779236114"
-          crossOrigin="anonymous"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
