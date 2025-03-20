2881b592d86bd4385e970d742d91525d0f9f5109
gr82b4tun8 <gr82b4tun8@gmail.com>
2025-03-20 14:36:39 +0000

    fixed ads????

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
+}
+// components/GoogleAdSense.js for auto ads only
+export default function GoogleAdsense() {
+  return (
+    <Script
+      id="adsbygoogle-init"
+      strategy="afterInteractive"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1549212779236114"
+      crossOrigin="anonymous"
+    />
+  );
 }
